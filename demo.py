2f7fb5c40cc77429a9abd4bc386c8f54eb29d491
Nandhagopal2912 <naanu2912@gmail.com>
2026-03-28 09:30:45 +0530

    added multiplication and division

--- demo.py
+++ demo.py
@@ -6,3 +6,8 @@ print("Sum of the two number is :")
 print(a+b)
 print("subraction of the two number is :")
 print(a-b)
+
+print("Multiplication of the two number is :")
+print(a*b)  
+print("Division of the two number is :")
+print(a/b)
